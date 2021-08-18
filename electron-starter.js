7ffb1face9c49f540893640e17047ab7d8c299c3
yifeng.tao <yifeng.tao@luckincoffee.com>
2021-08-18 17:48:06 +0800

    add electron

---
+++ electron-starter.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mianWindow;
+function createWindow() {
+  mianWindow = new BrowserWindow({
+    height: 800,
+    width: 800,
+  });
+
+  // 根据不同的环境设置文件指向
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, './build/index.html'),
+    protocol: 'file',
+    slashes: true
+  })
+  mianWindow.loadURL(startUrl);
+
+  mianWindow.webContents.openDevTools();
+  mianWindow.on('closed', () => {
+    mianWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (mianWindow === null) {
+    createWindow();
+  }
+})
+
