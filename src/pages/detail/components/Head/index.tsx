25ffb3dfb1f90597c76c203b752d2b923f528583
yifeng.tao <yifeng.tao@luckincoffee.com>
2021-10-28 18:07:11 +0800

    update music list header

--- src/pages/detail/components/Head/index.tsx
+++ src/pages/detail/components/Head/index.tsx
@@ -1,16 +1,90 @@
+import FontIcon from '../../../../components/FontIcon';
 import React from 'react';
+import './style.less';
+import { Button } from 'antd';
 
 interface HeadProps {
   picUrl: string,
   name: string,
   createAuthor: string,
   createTime: string,
-  tagName: string,
+  tagName: string[],
   songLen: number,
   playTimes: number,
   desc: string
 }
 
 const Head: React.FC<HeadProps> = (props) => {
-
+  return (
+    <div>
+      <header className="music-header_wrap">
+      <img
+        alt="title"
+        src={props.picUrl}
+      />
+      <div className="music-header_right">
+        <div className="music-header_line">
+          <span className="music-header_line_type">歌单</span>
+          <span className="music-header_line_name">
+            说不出名字的那些经典英文歌
+          </span>
+        </div>
+        <div className="music-header_line play_line">
+          <span>
+            {props.createAuthor}
+          </span>
+          <span>
+            {props.createTime}
+          </span>
+        </div>
+        <div className="music-header_line play_line">
+          <span className="music-header_line_btn">
+            <FontIcon className="icon" type="icon-bofang-copy"/>
+            播放全部
+          </span>
+          <span className="music-header_line_btn">
+            <FontIcon className="icon" type="icon-shoucang"/>
+            收藏(82万)
+          </span>
+          <span className="music-header_line_btn">
+            <FontIcon className="icon" type="icon-fenxiang"/>
+            分享(521)
+          </span>
+          <span className="music-header_line_btn">
+            <FontIcon className="icon" type="icon-xiazai"/>
+            下载全部
+          </span>
+        </div>
+        <div className="music-header_line">
+          <span>
+            标签
+          </span>
+          {
+            props.tagName.map(item => (
+              <Button type="link">{item}</Button>
+            ))
+          }
+        </div>
+        <div className="music-header_line">
+          <span>
+            歌曲数：{props.songLen}
+          </span>
+          <span>
+            播放数：{props.playTimes}
+          </span>
+        </div>
+        <div className="music-header_line">
+          <span>
+            简介：
+          </span>
+          <span>
+            {props.desc}
+          </span>
+        </div>
+      </div>
+    </header>
+    </div>
+  )
 }
+
+export default Head;
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useLocation } from 'react-router-dom';
+import Head from './components/Head';
+import './style.less'
 
 type DetailProps = {
   [t: string]: any
@@ -9,16 +11,20 @@ const Detail: React.FC<DetailProps> = (props) => {
   const location = useLocation();
 
   return (
-    <div>
-      <head>
-        <img 
-          alt="title"
-          src="http://p2.music.126.net/hG2GoCHzJTAHd47JhViSAw==/109951165802640781.jpg?param=140y140" 
-        />
-        <div>
-          
-        </div>
-      </head>
+    <div className="music-list_wapper">
+      <Head 
+        picUrl="http://p2.music.126.net/hG2GoCHzJTAHd47JhViSAw==/109951165802640781.jpg?param=140y140"
+        name="Test"
+        createAuthor="yeyu"
+        createTime="2021-10-28"
+        tagName={['欧美', 'R&B']}
+        songLen={10}
+        playTimes={1000}
+        desc='祝所有努力奋斗的人取得自己想要的成绩。'
+      />
+      <div>
+        {/* aaaaaa */}
+      </div>
     </div>
   )
 }
